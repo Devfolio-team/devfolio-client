957a1d122c52d8f74dfb9c204fa6163a449a270b
choisuhyeok1255 <csh300029@gmail.com>
2021-04-11 16:14:43 +0900

    feat: Heading 컴포넌트 완성

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { string, number, oneOf } from "prop-types";
+
+// as 사용해서 heading level을 유동적으로 사용
+const Heading = styled.h1`
+  color: ${(props) => props.fontColor};
+  font-size: ${(props) => props.fontSize}rem;
+  font-weight: ${(props) => props.fontWeight};
+  line-height: ${(props) => props.lineHeight}px;
+`;
+
+Heading.propTypes = {
+  /** Heading에 적용 할 color를 설정합니다.*/
+  color: string,
+  /** Heading에 적용 할 fontSize 설정합니다.*/
+  fontSize: number,
+  /** Heading에 적용 할 fontWeight를 설정합니다.*/
+  fontWeight: number,
+  /** Heading에 적용 할 lineHeight를 설정합니다.*/
+  lineHeight: number,
+  /** Heading에 적용 할 Heading-level을 설정합니다.*/
+  as: oneOf(["h1", "h2", "h3", "h4", "h5", "h6"]).isRequired,
+};
+
+export default Heading;
---
+++ src/components/Heading/Heading.stories.js
@@ -0,0 +1,28 @@
+import Heading from "./Heading";
+
+export default {
+  title: "Component/Heading",
+  componenet: Heading,
+  parameters: {
+    docs: {
+      discription: {
+        component:
+          "**Heading** 컴포넌트는 as props를 받아서 h1~h6 중 하나를 사용 할 수 있습니다.",
+      },
+    },
+  },
+  argTypes: {
+    color: { control: "color" },
+    fontSize: { control: "number" },
+    fontWeight: { control: "number" },
+    lineHeight: { control: "number" },
+  },
+};
+
+const Template = (args) => <Heading {...args} />;
+
+export const ExampleHeading = Template.bind({});
+
+ExampleHeading.args = {
+  children: "Example",
+};
