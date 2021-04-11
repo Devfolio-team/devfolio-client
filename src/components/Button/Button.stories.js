d17b16516b7651ae854decbf22fa28f0e893fbce
bcround <sbc1142@gmail.com>
2021-04-11 20:26:10 +0900

    Style: prettier 자동 수정 반영
    modified:   src/components/Button/Button.stories.js

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,9 +7,9 @@ export default {
     docs: {
       description: {
         component:
-          '**버튼** 컴포넌트는 클릭으로 사용자와 상호작용(interaction) 하여 제어(contorl) 하는 컨트롤입니다.'
-      }
-    }
+          '**버튼** 컴포넌트는 클릭으로 사용자와 상호작용(interaction) 하여 제어(contorl) 하는 컨트롤입니다.',
+      },
+    },
   },
   argTypes: {
     onClick: { action: '버튼 클릭' },
@@ -19,8 +19,8 @@ export default {
     height: { control: 'number' },
     fontSize: { control: 'number' },
     fontWeight: { control: 'number' },
-    borderRadius: { control: 'number' }
-  }
+    borderRadius: { control: 'number' },
+  },
 };
 
 const Template = args => <Button {...args} />;
@@ -28,5 +28,5 @@ const Template = args => <Button {...args} />;
 export const ExampleButton = Template.bind({});
 
 ExampleButton.args = {
-  children: 'Example'
+  children: 'Example',
 };
