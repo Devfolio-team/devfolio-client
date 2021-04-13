30a4178d303e17a8c1bd265dd951a6efb8d17b76
bcround <sbc1142@gmail.com>
2021-04-13 13:26:31 +0900

    Modify: 저장을 안하고 index.js 파일을 merge를 시켜서 저장후 다시 commit modified:   src/components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,9 +5,6 @@ export { default as RadioButton } from './RadioButton/RadioButton';
 export { default as Input } from './Input/Input';
 export { default as TextArea } from './TextArea/TextArea';
 export { default as Selectbox } from './Selectbox/Selectbox';
-<<<<<<< HEAD
 export { default as DND } from './DND/DND';
-=======
 export { default as Container } from './Container/Container';
 export { default as SVGIcon } from './SVGIcon/SVGIcon';
->>>>>>> efc59b4bf1711939974520f63937d135751fb6ce
