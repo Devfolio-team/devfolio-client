bb90756db01dba11c85963aa90c459d17f86a6be
choisuhyeok1255 <csh300029@gmail.com>
2021-04-13 13:32:36 +0900

    Refactor: Logo.js 코드 리팩토링
    삼항 연산자 코드에서 겹치는 부분 리팩토링
    	modified:   src/components/Logo/Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,31 +1,20 @@
-import styled from 'styled-components';
-import { number, string } from 'prop-types';
+import { string } from 'prop-types';
 import { ReactComponent as LogoBlack } from 'assets/LogoBlack.svg';
 import { ReactComponent as LogoWhite } from 'assets/LogoWhite.svg';
 
-const StyledLogoBlack = styled(LogoBlack)`
-  width: ${props => props.width}px;
-  height: ${props => props.heigth}px;
-`;
-
-const StyledLogoWhite = styled(LogoWhite)`
-  width: ${props => props.width}px;
-  height: ${props => props.heigth}px;
-`;
-
 const Logo = ({ type, width, height, ...restProps }) => {
-  return type === 'Black' ? (
-    <StyledLogoBlack width={width} height={height} {...restProps} />
-  ) : (
-    <StyledLogoWhite width={width} height={height} {...restProps} />
-  );
+  let Temp = null;
+
+  Temp = type === 'Black' ? LogoBlack : LogoWhite;
+
+  return <Temp width={width} height={height} {...restProps} />;
+};
+
+Logo.defaultProps = {
+  type: 'Black',
 };
 
 Logo.propTypes = {
-  /** Logo에 적용 할 width를 설정합니다. */
-  width: number,
-  /** Logo에 적용 할 height를 설정합니다. */
-  height: number,
   /** Logo에 적용 할 type를 설정합니다. */
   type: string.isRequired,
 };
