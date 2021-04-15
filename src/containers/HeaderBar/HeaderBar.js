807217edf87f514fd62285039120433b1c6c2181
choisuhyeok1255 <csh300029@gmail.com>
2021-04-15 13:29:49 +0900

    Feat: 로그인모달 컴포넌트 관련 모달창 on/off 이벤트 추가 	modified:   src/containers/HeaderBar/HeaderBar.js

--- src/containers/HeaderBar/HeaderBar.js
+++ src/containers/HeaderBar/HeaderBar.js
@@ -1,10 +1,10 @@
 import { A11yHidden, Button, Container, Heading, Logo, Portal } from 'components';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import useDetectViewport from 'hooks/useDetectViewport';
 import { LoginModalDialog } from 'containers';
 
-const StyledHeader = styled.header.attrs(props => {})`
+const StyledHeader = styled.header`
   width: 100vw;
   height: 64px;
   background: ${({ background }) => background};
@@ -18,9 +18,25 @@ const HeaderBar = () => {
   const viewport = useDetectViewport();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const ref = useRef(null);
+  const beforeRef = useRef(null);
 
-  const onModalClickHandler = () => {
-    setIsModalOpen(!isModalOpen);
+  const onModalOpenHandler = () => {
+    setIsModalOpen(true);
+  };
+
+  const onModalCloseHandler = e => {
+    if (e.keyCode === 27) {
+      setIsModalOpen(false);
+      beforeRef.current.focus();
+      return;
+    }
+
+    if (e.target === e.currentTarget) {
+      setIsModalOpen(false);
+      beforeRef.current.focus();
+      return;
+    }
   };
 
   const { isDesktop } = viewport;
@@ -49,13 +65,19 @@ const HeaderBar = () => {
           fontSize={1.6}
           borderRadius={16}
           border="0"
-          onClick={onModalClickHandler}
+          onClick={onModalOpenHandler}
+          ref={beforeRef}
         >
           로그인
         </Button>
         {isModalOpen ? (
           <Portal id="modal-root">
-            <LoginModalDialog onModalClickHandler={onModalClickHandler}></LoginModalDialog>
+            <LoginModalDialog
+              ref={ref}
+              onModalCloseHandler={onModalCloseHandler}
+              isModalOpen={isModalOpen}
+              setIsModalOpen={setIsModalOpen}
+            ></LoginModalDialog>
           </Portal>
         ) : null}
       </Container>
