eda079d533c658c1e946b2f83e6096933b7e97b3
choisuhyeok1255 <csh300029@gmail.com>
2021-04-15 23:03:29 +0900

    Modify: Logo storybook 수정
    흰색 Logo가 배경 떄문에 안보이는 현상이 있어서
    배경색을 변경해서 보이도록 수정
    	modified:   src/components/Logo/Logo.stories.js

--- src/components/Logo/Logo.stories.js
+++ src/components/Logo/Logo.stories.js
@@ -3,7 +3,15 @@ import Logo from './Logo';
 export default {
   title: 'Component/Logo',
   component: Logo,
+  decorators: [
+    Story => (
+      <div style={{ backgroundColor: '#888' }}>
+        <Story />
+      </div>
+    ),
+  ],
   parameters: {
+    layout: 'fullscreen',
     docs: {
       description: {
         component: '**Logo** 컴포넌트는 type을 받아서 Logo를 사용 할 수 있습니다.',
