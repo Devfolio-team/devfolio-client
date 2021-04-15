74a26252e05930ec90bc9d3a8c99b253b7b00d71
HaJunRyu <hjr4557@naver.com>
2021-04-16 01:04:54 +0900

    Feat: 홈페이지 개발 진행중... 메뉴바 레아아웃을 잡고 중간커밋 modified:   src/pages/Home/HomePage.js

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import styled from 'styled-components';
+import { Button, Container, MenuUnderline } from 'components';
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
 
-const StyledHomePage = styled.main``;
+const StyledHomePage = styled.main`
+  ${({ $padding, $background }) => css`
+    padding: ${$padding};
+    background: #f8f9fa;
+  `}
+`;
 
-const HomePage = () => {
-  return <StyledHomePage></StyledHomePage>;
+const HomePage = ({ viewport }) => {
+  const { type, isMobile, isDesktop } = viewport;
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    setProjects();
+  }, []);
+
+  return (
+    <StyledHomePage $padding={isDesktop ? '30px 70px' : '30px 30px'}>
+      <Container width={type === 'xl' ? 1440 : '100%'}>
+        <Container position="relative" margin="0 0 30px 0">
+          <Button
+            width={112}
+            height={48}
+            background="transparent"
+            border="0"
+            fontSize={1.9}
+            fontWeight={700}
+            // outline="none"
+          >
+            인기
+          </Button>
+          <Button color="#868E96" background="transparent" border="0" fontSize={1.9}>
+            최신
+          </Button>
+          {/* 상태를 보고 transform 조정 */}
+          <MenuUnderline
+            position="absolute"
+            bottom="0"
+            left="0"
+            transform="translate3D(0px, 0, 0)"
+          />
+        </Container>
+        <Container></Container>
+      </Container>
+    </StyledHomePage>
+  );
 };
 
 HomePage.defaultProps = {};
