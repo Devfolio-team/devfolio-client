e836b5d7b39ac4b2c12135ad2e84515686a70114
HaJunRyu <hjr4557@naver.com>
2021-04-15 12:45:45 +0900

    Feat: X 모양 SVGIcon 추가 및 SVGIcon들에게 fill속성을 줄 수 있게끔 변경 new file:   src/assets/X.svg modified:   src/components/SVGIcon/SVGIcon.js modified:   src/components/SVGIcon/SVGIcon.stories.js

--- src/components/SVGIcon/SVGIcon.js
+++ src/components/SVGIcon/SVGIcon.js
@@ -12,94 +12,141 @@ import { ReactComponent as HeartEmpty } from 'assets/HeartEmpty.svg';
 import { ReactComponent as HeartRed } from 'assets/HeartRed.svg';
 import { ReactComponent as Sad } from 'assets/Sad.svg';
 import { ReactComponent as Smile } from 'assets/Smile.svg';
+import { ReactComponent as X } from 'assets/X.svg';
 
 const StyledAngry = styled(Angry)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledBlog = styled(Blog)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledEmail = styled(Email)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledEmpty = styled(Empty)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledGithubWhite = styled(GithubWhite)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledGithubBlack = styled(GithubBlack)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledGoogle = styled(Google)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledHeartBlack = styled(HeartBlack)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledHeartEmpty = styled(HeartEmpty)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledHeartRed = styled(HeartRed)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledSad = styled(Sad)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
 const StyledSmile = styled(Smile)`
   width: ${props => props.width}px;
   height: ${props => props.heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
 `;
 
-const SVGIcon = ({ type, width, height, ...restProps }) => {
+const StyledX = styled(X)`
+  width: ${props => props.$width}px;
+  height: ${props => props.$heigth}px;
+  path {
+    fill: ${props => props.$fill};
+  }
+`;
+
+const SVGIcon = ({ type, width, height, fill, ...restProps }) => {
   switch (type) {
     default:
     case 'Angry':
-      return <StyledAngry width={width} height={height} {...restProps} />;
+      return <StyledAngry width={width} height={height} $fill={fill} {...restProps} />;
     case 'Blog':
-      return <StyledBlog width={width} height={height} {...restProps} />;
+      return <StyledBlog width={width} height={height} $fill={fill} {...restProps} />;
     case 'Email':
-      return <StyledEmail width={width} height={height} {...restProps} />;
+      return <StyledEmail width={width} height={height} $fill={fill} {...restProps} />;
     case 'Empty':
-      return <StyledEmpty width={width} height={height} {...restProps} />;
+      return <StyledEmpty width={width} height={height} $fill={fill} {...restProps} />;
     case 'GithubWhite':
-      return <StyledGithubWhite width={width} height={height} {...restProps} />;
+      return <StyledGithubWhite width={width} height={height} $fill={fill} {...restProps} />;
     case 'GithubBlack':
-      return <StyledGithubBlack width={width} height={height} {...restProps} />;
+      return <StyledGithubBlack width={width} height={height} $fill={fill} {...restProps} />;
     case 'Google':
-      return <StyledGoogle width={width} height={height} {...restProps} />;
+      return <StyledGoogle width={width} height={height} $fill={fill} {...restProps} />;
     case 'HeartBlack':
-      return <StyledHeartBlack width={width} height={height} {...restProps} />;
+      return <StyledHeartBlack width={width} height={height} $fill={fill} {...restProps} />;
     case 'HeartEmpty':
-      return <StyledHeartEmpty width={width} height={height} {...restProps} />;
+      return <StyledHeartEmpty width={width} height={height} $fill={fill} {...restProps} />;
     case 'HeartRed':
-      return <StyledHeartRed width={width} height={height} {...restProps} />;
+      return <StyledHeartRed width={width} height={height} $fill={fill} {...restProps} />;
     case 'Sad':
-      return <StyledSad width={width} height={height} {...restProps} />;
+      return <StyledSad width={width} height={height} $fill={fill} {...restProps} />;
     case 'Smile':
-      return <StyledSmile width={width} height={height} {...restProps} />;
+      return <StyledSmile width={width} height={height} $fill={fill} {...restProps} />;
+    case 'X':
+      return <StyledX $width={width} $heigth={height} $fill={fill} {...restProps} />;
   }
 };
 
@@ -114,6 +161,8 @@ SVGIcon.propTypes = {
   height: number,
   /** SVGIcon에 적용 할 type를 설정합니다. */
   type: string.isRequired,
+  /** SVGIcon에 적용 할 색상을 설정합니다. */
+  fill: string,
 };
 
 export default SVGIcon;
