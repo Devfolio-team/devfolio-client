33edc57267e3c5a5c3ee3b4dd9774d1bef8005ab
choisuhyeok1255 <csh300029@gmail.com>
2021-04-15 21:57:35 +0900

    Feat: SignupModalDialog Component 생성
    storybook은 파일만 추가
    	new file:   src/containers/SignupModalDialog/SignupModalDialog.js
    	new file:   src/containers/SignupModalDialog/SignupModalDialog.stories.js
    	modified:   src/containers/index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as LoginModalDialog } from './LoginModalDialog/LoginModalDialog';
 export { default as HeaderBar } from 'containers/HeaderBar/HeaderBar';
 export { default as FooterBar } from 'containers/FooterBar/FooterBar';
+export { default as SignupModalDialog } from './SignupModalDialog/SignupModalDialog';
