fcc8d65e7f8a6a95ad6b7fd63b48e46b7db7c12a
bcround <sbc1142@gmail.com>
2021-04-16 15:22:23 +0900

    Refactor: Selectbox 컴포넌트 상태 관리 중점으로 리팩토링하고 스토리북도 그에 대한 변경 사항 리팩토링 solved: #81 modified:   src/components/Selectbox/Selectbox.js modified:   src/components/Selectbox/Selectbox.stories.js

--- src/components/Selectbox/Selectbox.js
+++ src/components/Selectbox/Selectbox.js
@@ -1,33 +1,59 @@
-import styled from 'styled-components';
-import { string } from 'prop-types';
+import styled, { css } from 'styled-components';
+import { string, number, func } from 'prop-types';
 import { color } from 'utils';
 
-const StyledSelectbox = styled.select.attrs(({ id, name }) => ({
-  id,
-  name,
-}))`
+const StyledSelectbox = styled.select`
   width: 65px;
   height: 30px;
   border-radius: 5px;
   border: 1px solid ${color.lightGray};
   box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.1);
   background: ${color.white};
-  margin-left: 15px;
+  margin-right: 15px;
 `;
 
-const Selectbox = ({ id, name, label, ...restProps }) => {
+const StyledLabel = styled.label`
+  ${({ $fontSize, $fontWeight, $color }) => css`
+    font-size: ${$fontSize}rem;
+    font-weight: ${$fontWeight};
+    color: ${$color};
+  `}
+`;
+
+const Selectbox = ({
+  id,
+  name,
+  label,
+  value,
+  onChange,
+  fontSize,
+  fontWeight,
+  color,
+  ...restProps
+}) => {
   const optArray = Array.from({ length: 10 }, (_, i) => i + 1);
+
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-      <StyledSelectbox name={name} id={id} {...restProps}>
+      <StyledSelectbox name={name} id={id} value={value} onChange={onChange} {...restProps}>
         <option value="">인원 수</option>
-        {optArray.map(optNum => (
-          <option value={optNum}>{optNum + '명'}</option>
+        {optArray.map((optNum, index) => (
+          <option value={optNum} key={index}>
+            {optNum + '명'}
+          </option>
         ))}
       </StyledSelectbox>
+      <StyledLabel htmlFor={id} $fontSize={fontSize} $fontWeight={fontWeight} $color={color}>
+        {label}
+      </StyledLabel>
     </>
   );
+};
+
+Selectbox.defaultProps = {
+  label: 'Example',
+  id: 'exSelectbox1',
+  name: 'exSelectbox',
 };
 
 Selectbox.propTypes = {
@@ -37,6 +63,16 @@ Selectbox.propTypes = {
   name: string.isRequired,
   /** 셀렉트 박스의 레이블을 설정합니다. */
   label: string,
+  /** 셀렉트 박스의 선택된 값을 설정합니다. */
+  value: number,
+  /** 셀렉트 박스의 변경되는 값을 감지하는 이벤트를 설정합니다. */
+  onChange: func,
+  /** 셀렉트 박스의 레이블의 폰트 크기를 설정합니다. */
+  fontSize: number,
+  /** 셀렉트 박스의 레이블의 폰트 두께를 설정합니다. */
+  fontWeight: string,
+  /** 셀렉트 박스의 레이블의 폰트 색상을 설정합니다. */
+  color: string,
 };
 
 export default Selectbox;
