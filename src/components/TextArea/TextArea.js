6217d5850dc0ca7ec11da601a4983080aefb0fe2
bcround <sbc1142@gmail.com>
2021-04-17 19:35:49 +0900

    Modify: value와 onChange 상태끌어올리기에 사용하기위에 props추가 하고 placeholder 위치 변경가능하게끔 props 4개 추가 modified:   src/components/TextArea/TextArea.js modified:   src/components/TextArea/TextArea.stories.js

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -1,17 +1,23 @@
 import styled from 'styled-components';
-import { number, string } from 'prop-types';
+import { number, string, func } from 'prop-types';
 import { useState } from 'react';
+import { color } from 'utils';
+import { A11yHidden } from 'components';
 
-const Styledlabel = styled.label.attrs(({ htmlFor }) => ({
+const StyledLabel = styled.label.attrs(({ htmlFor }) => ({
   htmlFor,
 }))`
   display: block;
   transition: 0.4s;
-  transform: translateY(${({ focus, areaValue }) => (focus || areaValue ? -0.5 : 3.5)}rem);
+  transform: translateY(
+    ${({ focus, areaValue, beforeTranslate, afterTranslate }) =>
+      focus || areaValue ? afterTranslate : beforeTranslate}rem
+  );
   font-size: 2rem;
   margin-top: 15px;
-  margin-left: ${({ focus, areaValue }) => (focus || areaValue ? 0 : 15)}px;
-  color: #7e7272;
+  margin-left: ${({ focus, areaValue, beforeMargin, afterMargin }) =>
+    focus || areaValue ? afterMargin : beforeMargin}px;
+  color: ${color.placeholder};
 `;
 
 const StyledTextArea = styled.textarea`
@@ -24,27 +30,48 @@ const StyledTextArea = styled.textarea`
   border: 0;
 `;
 
-const TextArea = ({ id, width, height, color, ...restProps }) => {
-  const [focus, setFocus] = useState(false);
-  const [areaValue, setAreaValue] = useState('');
+const TextArea = ({
+  id,
+  width,
+  height,
+  color,
+  value,
+  label,
+  mode,
+  onChange,
+  beforeTranslate,
+  afterTranslate,
+  beforeMargin,
+  afterMargin,
+  ...restProps
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
 
   const onFocusHandler = () => {
-    setFocus(true);
+    setIsFocused(true);
   };
 
   const onBlurHandler = e => {
-    setFocus(false);
-    if (areaValue !== '' && focus === false) setAreaValue(true);
+    setIsFocused(false);
   };
 
-  const onChangeHandler = e => {
-    setAreaValue(e.target.value);
-  };
   return (
     <>
-      <Styledlabel for={id} focus={focus} areaValue={areaValue}>
-        간단한 자기소개
-      </Styledlabel>
+      {mode === 'hidden' ? (
+        <A11yHidden as="label" htmlFor={id} children={label} />
+      ) : (
+        <StyledLabel
+          htmlFor={id}
+          focus={isFocused}
+          areaValue={value}
+          beforeTranslate={beforeTranslate}
+          afterTranslate={afterTranslate}
+          beforeMargin={beforeMargin}
+          afterMargin={afterMargin}
+        >
+          {label}
+        </StyledLabel>
+      )}
       <StyledTextArea
         id={id}
         $width={width}
@@ -52,20 +79,38 @@ const TextArea = ({ id, width, height, color, ...restProps }) => {
         {...restProps}
         onFocus={onFocusHandler}
         onBlur={onBlurHandler}
-        onChange={onChangeHandler}
-        value={areaValue}
+        onChange={onChange}
+        value={value}
       />
     </>
   );
 };
 
 TextArea.propTypes = {
+  /** TextArea와 label에 적용 할 공통의 ID를 설정합니다. */
+  id: string.isRequired,
   /** TextArea 적용 할 width 크기를 설정합니다. */
   width: number.isRequired,
   /** TextArea 적용 할 height 크기를 설정합니다. */
   height: number.isRequired,
-  /** TextArea와 label에 적용 할 공통의 ID를 설정합니다. */
-  id: string.isRequired,
+  /** TextArea 폰트색을 설정합니다. */
+  color: string,
+  /** TextArea 박스 입력되는 값을 설정합니다. */
+  value: string,
+  /** TextArea의 label값을 설정합니다. */
+  label: string,
+  /** TextArea의 레이블의 숨김처리를 설정합니다. */
+  mode: string,
+  /** TextArea의 변경되는 값을 감지하는 이벤트를 설정합니다. */
+  onChange: func,
+  /** TextArea의 placeholder의 기본 위치(위 아래로 이동)를 설정합니다. */
+  beforeTranslate: number,
+  /** TextArea의 placeholder의 움직일 위치(위 아래로 이동)를 설정합니다. */
+  afterTranslate: number,
+  /** TextArea의 placeholder 기본 위치(좌우로 이동) 설정 합니다. */
+  beforeMargin: number,
+  /** TextArea의 placeholder 움직일 위치(좌우로 이동) 설정합니다. */
+  afterMargin: number,
 };
 
 export default TextArea;
