c2c654c520d6847353db2a21024c632e7df65d28
choisuhyeok1255 <csh300029@gmail.com>
2021-04-17 15:31:37 +0900

    Feat: 접근성, 반응형 디자인 추가
    반응형 -> height, margin-bottom 속성 변경
    ref.current.focus() 모달창 생성 시 바로 dialog로 포커스
    aria 접근성 추가
    	modified:   src/containers/LoginModalDialog/LoginModalDialog.js

--- src/containers/LoginModalDialog/LoginModalDialog.js
+++ src/containers/LoginModalDialog/LoginModalDialog.js
@@ -1,31 +1,34 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Button, Container, Heading, Input, SVGIcon, Modal, Dialog } from 'components';
 import { forwardRef, useEffect, React } from 'react';
 
 const DivisionLine = styled.div`
-  border-bottom: 1px solid #454b58;
-  width: 100%;
-  height: 44px;
-  margin-bottom: 42px;
+  ${({ $height, $marginBottom }) => css`
+    border-bottom: 1px solid #454b58;
+    width: 100%;
+    height: ${$height};
+    margin-bottom: ${$marginBottom};
+  `}
 `;
 
 const DivisionDiv = styled.div`
-  color: #ffffff;
-  background-color: #2c3035;
-  width: 146px;
-  height: 46px;
-  line-height: 46px;
-  text-align: center;
-  margin: 0 auto;
-  font-size: 16px;
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
+  ${({ $width }) => css`
+    color: #ffffff;
+    background-color: #2c3035;
+    width: ${$width};
+    height: 46px;
+    line-height: 46px;
+    text-align: center;
+    font-size: 16px;
+    position: absolute;
+    margin: 0 auto;
+    left: 50%;
+    transform: translateX(-50%);
+  `};
 `;
 
 const DialogForm = styled.form`
-  width: 650px;
-  height: 448px;
+  width: 100%;
 `;
 
 const IconContainer = styled.div`
@@ -43,11 +46,14 @@ const onSubmitHandler = e => {
   e.preventDefault();
 };
 
-const LoginModalDialog = forwardRef(({ onModalCloseHandler }, ref) => {
+const LoginModalDialog = forwardRef(({ onModalCloseHandler, viewport }, ref) => {
+  const { isDesktop, isMobile, type } = viewport;
   const $rootNode = document.getElementById('root');
   $rootNode.setAttribute('aria-hidden', 'true');
 
   useEffect(() => {
+    ref.current.focus();
+
     const handleFocusTrap = e => {
       const dialogNode = ref.current;
       const focusableNodeList = dialogNode.querySelectorAll(
@@ -81,7 +87,15 @@ const LoginModalDialog = forwardRef(({ onModalCloseHandler }, ref) => {
   return (
     <>
       <Modal onClick={onModalCloseHandler}>
-        <Dialog ref={ref} role="dialog" width={710} height={500} borderRadius={8}>
+        <Dialog
+          ref={ref}
+          role="dialog"
+          width={`${isDesktop ? '710px' : type === 'sm' ? '80%' : '266px'}`}
+          height={`${isDesktop ? '500px' : type === 'sm' ? '500px' : '420px'}`}
+          padding={`${isDesktop ? '30px' : type === 'sm' ? '50px' : '30px'}`}
+          borderRadius={8}
+          tabIndex={0}
+        >
           <Heading
             as={'h2'}
             fontSize={3.5}
@@ -91,13 +105,19 @@ const LoginModalDialog = forwardRef(({ onModalCloseHandler }, ref) => {
           >
             시작하기
           </Heading>
-          <Container width={550} display="flex" justifyContent="space-between">
+          <Container
+            width={`${isDesktop ? '550px' : '100%'}`}
+            display={`${isDesktop ? 'flex' : ''}`}
+            justifyContent={`${isDesktop ? 'space-between' : ''}`}
+            flexFlow={`${isDesktop ? '' : 'column'}`}
+          >
             <Button
-              ariaLabel="구글 로그인"
-              width={260}
-              height={66}
+              aria-label="구글 로그인"
+              width={`${isDesktop ? '47%' : '100%'}`}
+              height={`${type === 'xs' ? '48px' : '66px'}`}
               borderRadius={30}
               bgColor={'#ffffff'}
+              margin={`${isMobile ? '0 0 10px 0' : ''}`}
             >
               <IconContainer>
                 <SVGIcon type={'Google'} width="30px" height="30px"></SVGIcon>
@@ -105,11 +125,12 @@ const LoginModalDialog = forwardRef(({ onModalCloseHandler }, ref) => {
               </IconContainer>
             </Button>
             <Button
-              ariaLabel="깃허브 로그인"
-              width={260}
-              height={66}
+              aria-label="깃허브 로그인"
+              width={`${isDesktop ? '47%' : '100%'}`}
+              height={`${type === 'xs' ? '48px' : '66px'}`}
               borderRadius={30}
               bgColor={'#ffffff'}
+              margin={`${isMobile ? '0 0 10px 0' : ''}`}
             >
               <IconContainer>
                 <SVGIcon type={'GithubBlack'} width="30px" height="30px"></SVGIcon>
@@ -118,33 +139,41 @@ const LoginModalDialog = forwardRef(({ onModalCloseHandler }, ref) => {
             </Button>
           </Container>
           <Container display="flex" alignItems="center" position="relactive">
-            <DivisionDiv>또는</DivisionDiv>
-            <DivisionLine></DivisionLine>
+            <DivisionDiv $width={`${isDesktop ? '146px' : type === 'sm' ? '80px' : '80px'}`}>
+              또는
+            </DivisionDiv>
+            <DivisionLine
+              $height={`${isDesktop ? '44px' : type === 'sm' ? '20px' : '20px'}`}
+              $marginBottom={`${isDesktop ? '42px' : type === 'sm' ? '20px' : '20px'}`}
+            ></DivisionLine>
           </Container>
           <DialogForm onSubmit={onSubmitHandler}>
             <Input
               id={'dialogInput'}
+              aria-label="이메일 로그인"
               label="이메일 주소를 입력해주세요 :)"
-              width={550}
-              height={65}
+              autoComplete="off"
+              labelsize={`${isDesktop ? '2' : type === 'sm' ? '1.5' : '1.1'}`}
+              width={`${isDesktop ? '550px' : type === 'sm' ? '100%' : '100%'}`}
+              height={`${type === 'xs' ? '48px' : '66px'}`}
               borderRadius={30}
               margin="0 auto"
               padding="21px 0 21px 26px"
               display="block"
-              beforeTranslate="4.2"
-              afterTranslate="-1"
-              beforeMargin="80"
-              afterMargin="60"
+              beforeTranslate={`${isDesktop ? '4.2' : type === 'sm' ? '4' : '3'}`}
+              afterTranslate={`${isDesktop ? '-1.5' : type === 'sm' ? '-1' : '-1'}`}
+              beforeMargin={`${isDesktop ? '80' : type === 'sm' ? '20' : '20'}`}
+              afterMargin={`${isDesktop ? '60' : type === 'sm' ? '4.2' : '10'}`}
             />
             <Button
-              width={260}
-              height={65}
+              width={`${isDesktop ? '195px' : '100%'}`}
+              height={`${type === 'xs' ? '48px' : '65px'}`}
               background={'#2c3035'}
               border="1px solid #ffffff"
               borderRadius={30}
               color={'#ffffff'}
               display="block"
-              margin="55px auto 0 auto"
+              margin={`${isDesktop ? '55px auto 0 auto' : '10px 0 0 0 '}`}
             >
               로그인 / 회원가입
             </Button>
