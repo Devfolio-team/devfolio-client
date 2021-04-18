0aed211eeb5fc575506c2505cafa580615b027dc
HaJunRyu <hjr4557@naver.com>
2021-04-19 02:42:28 +0900

    Feat: 애플리케이션에 redux store를 index.js에서 최상단에 적용 modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import { render } from 'react-dom';
 import './styles/index.scss';
 
 import App from './app/App';
+import StoreProvider from 'store/store';
 
 render(
   <StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </StrictMode>,
   document.getElementById('root')
 );
