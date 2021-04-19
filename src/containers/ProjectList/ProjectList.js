15e723e4161bf8bd795e992172a6940a24f3c243
HaJunRyu <hjr4557@naver.com>
2021-04-19 15:56:59 +0900

    Feat: ProjectList 컨테이너 컴포넌트 스토리북 작성 modified:   src/containers/ProjectList/ProjectList.js modified:   src/containers/ProjectList/ProjectList.stories.js

--- src/containers/ProjectList/ProjectList.js
+++ src/containers/ProjectList/ProjectList.js
@@ -2,6 +2,7 @@ import { styled } from '@storybook/theming';
 import React from 'react';
 import { css } from 'styled-components';
 import { applyStyle } from 'utils';
+import { node } from 'prop-types';
 
 const StyledProjectList = styled.ul`
   ${props =>
@@ -12,6 +13,11 @@ const StyledProjectList = styled.ul`
 
 const ProjectList = ({ children }) => {
   return <StyledProjectList>{children}</StyledProjectList>;
+};
+
+ProjectList.propTypes = {
+  /** 리스트들의 아이템 요소들을 받습니다. 자식 요소로는 무조건 li요소가 위치해야 합니다. */
+  children: node,
 };
 
 export default ProjectList;
