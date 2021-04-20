1aed029c115aec6b145fe313caccbc58a49aba91
choisuhyeok1255 <csh300029@gmail.com>
2021-04-20 21:41:38 +0900

    Style: props, propTypes 추가 	modified:   src/components/Heading/Heading.js

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -17,6 +17,9 @@ const Heading = ({
   background,
   margin,
   textAlign,
+  border,
+  borderBottom,
+  width,
   ...restProps
 }) => (
   <StyledHeading
@@ -27,6 +30,9 @@ const Heading = ({
     $background={background}
     $margin={margin}
     $textAlign={textAlign}
+    $border={border}
+    $borderBottom={borderBottom}
+    $width={width}
     {...restProps}
   ></StyledHeading>
 );
@@ -36,20 +42,26 @@ Heading.defaultProps = {
 };
 
 Heading.propTypes = {
-  /** Heading에 적용 할 color를 설정합니다. */
+  /** Heading에 적용 할 글자색을 설정합니다. */
   color: string,
-  /** Heading에 적용 할 fontSize 설정합니다. */
-  fontSize: number,
-  /** Heading에 적용 할 fontWeight를 설정합니다. */
+  /** Heading에 적용 할 글자 크기 설정합니다. */
+  fontSize: oneOfType([string, number]),
+  /** Heading에 적용 할 글자 굵기를 설정합니다. */
   fontWeight: number,
-  /** Heading에 적용 할 lineHeight를 설정합니다. */
+  /** Heading에 적용 할 줄 높이를 설정합니다. */
   lineHeight: oneOfType([string, number]),
-  /** Heading에 적용 할 backgroundColor를 설정합니다. */
+  /** Heading에 적용 할 배경색을 설정합니다. */
   backgroundColor: string,
   /** Heading에 적용 할 Heading-level을 설정합니다. */
   as: oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
-  /** Heading에 적용 할 margin을 설정합니다. */
+  /** Heading에 적용 할 바깥족 여백을 설정합니다. */
   margin: string,
+  /** Heading에 적용 할 테두리 선을 설정합니다. */
+  border: string,
+  /** Heading에 적용 할 테두리 선을 설정합니다. */
+  borderBottom: string,
+  /** Heading에 적용 할 가로크기를 설정합니다. */
+  width: string,
 };
 
 export default Heading;
