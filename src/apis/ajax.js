1f251dbf3ec721aa8c9db050d586876c07d08a8b
bcround <sbc1142@gmail.com>
2021-04-21 20:30:08 +0900

    Feat: apis 폴더내에서 project파일 내부에 project 데이터를 가지고 데이터베이스에 저장하는 postProject async 함수 생성 modified:   src/apis/ajax.js modified:   src/apis/project.js

--- src/apis/ajax.js
+++ src/apis/ajax.js
@@ -1,11 +1,12 @@
 import { signIn } from 'apis/auth';
-import { fetchProjects } from 'apis/project';
+import { fetchProjects, postProject } from 'apis/project';
 import { postImage } from 'apis/image';
 
 const ajax = {
   signIn,
   fetchProjects,
   postImage,
+  postProject,
 };
 
 export default ajax;
--- src/apis/project.js
+++ src/apis/project.js
@@ -2,5 +2,13 @@ import axios from 'axios';
 const API_URL = 'http://devfolio.world:3020';
 
 export const fetchProjects = async () => {
-  return await axios.get(`${API_URL}/api/project`);
+  return await axios.get(`${API_URL}/api/projects`);
+};
+
+export const postProject = async projectData => {
+  return await axios.post(`${API_URL}/api/project`, projectData, {
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
 };
