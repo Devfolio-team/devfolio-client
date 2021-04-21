5e079603df703b0518dda5a6e89ab3626cac023c
choisuhyeok1255 <csh300029@gmail.com>
2021-04-21 23:58:08 +0900

    Refactor: project -> projects로 수정 	modified:   src/apis/project.js

--- src/apis/project.js
+++ src/apis/project.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 const API_URL = 'http://devfolio.world:3020';
 
 export const fetchProjects = async () => {
-  return await axios.get(`${API_URL}/api/project`);
+  return await axios.get(`${API_URL}/api/projects`);
 };
