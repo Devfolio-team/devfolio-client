a6b27cb730c09a70503fb46df5a1c3ef0316be7f
bcround <sbc1142@gmail.com>
2021-04-21 15:28:25 +0900

    Feat: ProjectEditForm 컨테이너 생성 및 ProjectEditPage에 연결 modified:   src/containers/ProjectEditForm/ProjectEditForm.js modified:   src/pages/ProjectEdit/ProjectEditPage.js

--- src/pages/ProjectEdit/ProjectEditPage.js
+++ src/pages/ProjectEdit/ProjectEditPage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ProjectEditForm } from 'containers';
+import { Heading } from 'components';
 
 const StyledProjectEditPage = styled.main``;
 
 const ProjectEditPage = () => {
   return (
     <StyledProjectEditPage>
+      <Heading as="h2" color="#212121" fontSize={3} textAlign="center" lineHeight="46px">
+        안녕하세요!
+        <br />
+        프로젝트 등록을 시작해 볼까요?
+      </Heading>
       <ProjectEditForm />
     </StyledProjectEditPage>
   );
