5919ca1084f8fa04a516f4b3fd1021246a9da9c3
bcround <sbc1142@gmail.com>
2021-04-21 21:43:50 +0900

    project.js를 저장하지않고 git add commit을 해서 다시 저장후 commit modified:   src/apis/project.js

--- src/apis/project.js
+++ src/apis/project.js
@@ -3,7 +3,6 @@ const API_URL = 'http://devfolio.world:3020';
 
 export const fetchProjects = async () => {
   return await axios.get(`${API_URL}/api/projects`);
-<<<<<<< HEAD
 };
 
 export const postProject = async projectData => {
@@ -12,6 +11,4 @@ export const postProject = async projectData => {
       'Content-type': 'application/json',
     },
   });
-=======
->>>>>>> dc465351ec81863fc0235796e993deaef1c38941
 };
