e52a1bc92ba1f866e920f28439e7a4a6f22cccfd
bcround <sbc1142@gmail.com>
2021-04-21 21:16:47 +0900

    Feat: 프로젝트 등록 페이지에서 Redux에 저장된 인증된 사용자의 이름을 가져와 화면 상단에 유저이름 표시 구현 및 프로젝트 등록 페이지에서 인풋에 작성한 내용들 데이터베이스에 post요청 보내도록 구현하고 submit버튼을 클릭했을시에 등록한 프로젝트를 조회할 수 있는 페이지로 바로 넘어가도록 history push 설정
    solved: #132
    modified:   src/containers/ProjectEditForm/ProjectEditForm.js
    modified:   src/pages/ProjectEdit/ProjectEditPage.js

--- src/pages/ProjectEdit/ProjectEditPage.js
+++ src/pages/ProjectEdit/ProjectEditPage.js
@@ -2,14 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import { ProjectEditForm } from 'containers';
 import { Heading } from 'components';
+import { useSelector } from 'react-redux';
 
 const StyledProjectEditPage = styled.main``;
 
 const ProjectEditPage = () => {
+  const userName = useSelector(state => state.auth.currentUser.name);
   return (
     <StyledProjectEditPage>
       <Heading as="h2" color="#212121" fontSize={3} textAlign="center" lineHeight="46px">
-        안녕하세요!
+        {userName}님 안녕하세요!
         <br />
         프로젝트 등록을 시작해 볼까요?
       </Heading>
