f4458c7855fad3868be0cf248086a42545b832ee
HaJunRyu <hjr4557@naver.com>
2021-04-24 21:14:39 +0900

    Feat: 포트폴리오 페이지의 Skeleton UI 구현 및 Skeleton 스토리북 작성 modified:   package-lock.json modified:   package.json new file:   src/components/Skeleton/Skeleton.stories.js modified:   src/containers/PortfolioProfile/PortfolioProfile.js modified:   src/pages/Portfolio/PortfolioPage.js

--- src/containers/PortfolioProfile/PortfolioProfile.js
+++ src/containers/PortfolioProfile/PortfolioProfile.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { applyStyle } from 'utils';
 import Typewiter from 'react-simple-typewriter';
+import Skeleton from '@yisheng90/react-loading';
 
 const StyledPortfolioProfile = styled.div`
   ${props => css`
@@ -11,7 +12,7 @@ const StyledPortfolioProfile = styled.div`
     position: fixed;
     top: 0;
     left: 0;
-    z-index: 0;
+    z-index: -100;
     inset: 0px;
     width: 100%;
     min-height: 100vh;
@@ -51,7 +52,7 @@ const CustomTypewriter = styled.div`
   & > .typewriterEffectWraper > span:last-of-type {
     animation: cursorAnimation 1s infinite;
   }
-  line-height: 3rem;
+  line-height: 3.3rem;
   margin: 65px 0 65px;
   font-size: 3rem;
   font-weight: 700;
@@ -67,10 +68,10 @@ const CustomTypewriter = styled.div`
   }
 `;
 
-const PortfolioProfile = ({ userInfo, ...restProps }) => {
+const PortfolioProfile = ({ userInfo, skills, ...restProps }) => {
   const { vw, vh } = useDetectViewport();
 
-  const [iconSize, setIconSize] = useState(40);
+  const [iconSize, setIconSize] = useState(30);
 
   useEffect(() => {
     if (vw <= 1024) setIconSize(30);
@@ -94,50 +95,109 @@ const PortfolioProfile = ({ userInfo, ...restProps }) => {
         left="50%"
         transform="translate3D(-50%, -50%, 0)"
       >
-        <Image
-          src={
-            userInfo
-              ? userInfo.profile_photo
-              : 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'
-          }
-          alt=""
-          borderRadius="50%"
-          width={vw >= 1024 ? 250 : vw >= 768 ? 200 : 180}
-          height={vw >= 1024 ? 250 : vw >= 768 ? 200 : 180}
-          objectFit="cover"
-        />
-        <Span
-          fontSize={vw >= 1024 ? 4.8 : 3.2}
-          fontWeight={700}
-          color="#212121"
-          margin="35px 0 20px"
-        >
-          {userInfo ? userInfo.name : null}
-        </Span>
+        {userInfo ? (
+          <Image
+            src={
+              userInfo
+                ? userInfo.profile_photo
+                : 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'
+            }
+            alt=""
+            borderRadius="50%"
+            width={vw >= 1024 ? 250 : vw >= 768 ? 200 : 180}
+            height={vw >= 1024 ? 250 : vw >= 768 ? 200 : 180}
+            objectFit="cover"
+          />
+        ) : (
+          <Skeleton
+            color="#cccccc"
+            width={vw >= 1024 ? 250 : vw >= 768 ? 200 : 180}
+            height={vw >= 1024 ? 250 : vw >= 768 ? 200 : 180}
+            circle
+            translucent
+          />
+        )}
+
+        {userInfo ? (
+          <Span
+            fontSize={vw >= 1024 ? 4.8 : 3.2}
+            fontWeight={700}
+            color="#212121"
+            margin="35px 0 20px"
+          >
+            {userInfo.name}
+          </Span>
+        ) : (
+          <Span margin="35px 0 16px">
+            <Skeleton
+              color="#cccccc"
+              width={vw >= 1024 ? 150 : 100}
+              height={vw >= 1024 ? 48 : 32}
+              translucent
+            />
+          </Span>
+        )}
 
         <Span fontSize={vw >= 1024 ? 3 : 2} fontWeight={700} color="#212121">
-          {userInfo ? `(${userInfo.nickname})` : null}
+          {userInfo ? (
+            `(${userInfo.nickname})`
+          ) : (
+            <Skeleton
+              color="#cccccc"
+              width={vw >= 1024 ? 105 : 70}
+              height={vw >= 1024 ? 30 : 20}
+              translucent
+            />
+          )}
         </Span>
 
         <CustomTypewriter>
-          저는{' '}
-          <Container
-            className="typewriterEffectWraper"
-            display={vw >= 768 ? 'inline-block' : 'block'}
-          >
-            <Typewiter
-              loop
-              cursor
-              typeSpeed={150}
-              deleteSpeed={140}
-              delaySpeed={1000}
-              words={['React.js', 'Vue.js', 'Styled Components', 'Javascript']}
-            />{' '}
-          </Container>
-          할 줄 아는 개발자 입니다.
+          {skills ? (
+            <>
+              저는{' '}
+              <Container
+                className="typewriterEffectWraper"
+                display={vw >= 768 ? 'inline-block' : 'block'}
+              >
+                <Typewiter
+                  loop
+                  cursor
+                  typeSpeed={150}
+                  deleteSpeed={140}
+                  delaySpeed={1000}
+                  words={['React.js', 'Vue.js', 'Styled Components', 'Javascript']}
+                />{' '}
+              </Container>
+              할 줄 아는 개발자 입니다.
+            </>
+          ) : (
+            <Span margin="0 0 -4px">
+              {vw >= 768 ? (
+                <Skeleton
+                  color="#cccccc"
+                  width={vw >= 1024 ? 750 : 600}
+                  height={vw >= 1024 ? 36 : 36}
+                  translucent
+                />
+              ) : (
+                <Skeleton rows={3} color="#cccccc" width={250} height={33} translucent />
+              )}
+            </Span>
+          )}
         </CustomTypewriter>
 
-        <SimpleIntroduce>{userInfo ? userInfo.simple_introduction : null}</SimpleIntroduce>
+        {userInfo ? (
+          <SimpleIntroduce>{userInfo.simple_introduction}</SimpleIntroduce>
+        ) : (
+          <Container margin="-5px 0 -8px">
+            <Skeleton
+              color="#cccccc"
+              width={vw >= 1024 ? 480 : vw >= 768 ? 480 : vw >= 480 ? 400 : vw >= 365 ? 315 : 260}
+              height={vw >= 1024 ? 30 : vw >= 768 ? 30 : 25}
+              translucent
+            />
+          </Container>
+        )}
 
         <Container
           width={vw >= 1024 ? 250 : vw >= 270 ? 180 : '100%'}
