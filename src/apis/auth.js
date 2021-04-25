9bcd217d427d83d3156ff492ca2df7144350d276
HaJunRyu <hjr4557@naver.com>
2021-04-25 18:02:42 +0900

    process.env파일로 auth api요청 url 설정 modified:   src/apis/auth.js modified:   src/apis/image.js modified:   src/apis/portfolio.js modified:   src/apis/project.js modified:   src/containers/LoginModalDialog/LoginModalDialog.js

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const signIn = async auth_token => {
-  return await axios.post('http://localhost:3020/auth/signin', { authentication: auth_token });
+  return await axios.post(`${process.env.REACT_APP_API_URL}/auth/signin`, {
+    authentication: auth_token,
+  });
 };
