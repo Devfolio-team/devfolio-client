293812e234741056aa65fef427aa24cc5f1b9341
bcround <sbc1142@gmail.com>
2021-04-26 15:45:16 +0900

    Bugfix: GithubRepo가 validation이 완벽히 안되는 버그 해결 (정규표현식 수정함) solved: #181 modified:   src/components/GithubRepoInput/GithubRepoInput.js modified:   src/utils/validationSchema.js

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -16,7 +16,7 @@ const validationSchema = Yup.object({
     '형식에 맞는 URL을 작성해주세요!'
   ),
   githubUrl: Yup.string().matches(
-    /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+    /^https:\/\/github.com\/[\w-]+\/[\w-]+$/,
     '형식에 맞는 URL을 작성해주세요!'
   ),
   thumbnail: Yup.mixed()
