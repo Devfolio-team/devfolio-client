84321aca458c6cd60c49981b593a0b1274601ddb
bcround <sbc1142@gmail.com>
2021-05-26 13:53:40 +0900

    Modify: margin-top값을 주었음 modified:   src/components/GithubRepoInput/GithubRepoInput.js modified:   src/components/TechStacks/TechStacks.js

--- src/components/GithubRepoInput/GithubRepoInput.js
+++ src/components/GithubRepoInput/GithubRepoInput.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 const StyledContainer = styled(Container)`
   justify-self: end;
   width: 100%;
+  margin-top: 50px;
 
   @media (max-width: 1280px) {
     margin: 0 auto 60px;
