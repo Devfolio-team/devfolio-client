a53326f212f8423da8b76ace9100724282134a57
HaJunRyu <hjr4557@naver.com>
2021-08-09 23:33:06 +0900

    Refactor: lazy loading으로 코드 스플리팅 적용
    Changes to be committed:
    modified:   src/app/App.js
    deleted:    src/pages/index.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,8 @@
 import { FooterBar, HeaderBar } from 'containers';
 import { Redirect, Route, Switch, useLocation } from 'react-router';
-import {
-  FavoriteProjectPage,
-  HomePage,
-  PageNotFound,
-  PortfolioEditPage,
-  PortfolioPage,
-  ProjectEditPage,
-  ProjectPage,
-  SignIn,
-  SearchPage,
-} from 'pages';
+
 import useDetectViewport from 'hooks/useDetectViewport';
-import { useEffect } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { signOutMiddleware } from 'store/modules/auth/authMiddleware';
@@ -39,6 +29,16 @@ const App = () => {
 
   const isInfinityScrollPage = /^\/favorite_project\//.test(pathname) || pathname === '/';
 
+  const Home = lazy(() => import('pages/Home/HomePage'));
+  const Portfolio = lazy(() => import('pages/Portfolio/PortfolioPage'));
+  const PortfolioEdit = lazy(() => import('pages/PortfolioEdit/PortfolioEditPage'));
+  const Project = lazy(() => import('pages/Project/ProjectPage'));
+  const ProjectEdit = lazy(() => import('pages/ProjectEdit/ProjectEditPage'));
+  const FavoriteProject = lazy(() => import('pages/FavoriteProject/FavoriteProjectPage'));
+  const Search = lazy(() => import('pages/Search/SearchPage'));
+  const SignIn = lazy(() => import('pages/SignIn/SignIn'));
+  const PageNotFound = lazy(() => import('pages/PageNotFound/PageNotFound'));
+
   useEffect(() => {
     // 전역 상태에 유저의 정보는 있는데 쿠키에 인증토큰이 존재하지 않는다면 로그아웃을 시켜 전역상태를 비워준다. (쿠키만 없을때)
     if (auth.currentUser && !new RegExp(/auth_token/).test(document.cookie)) {
@@ -50,26 +50,20 @@ const App = () => {
     <FlexApp>
       <HeaderBar viewport={viewport} />
       <EmptyContainer>
-        <Switch>
-          <Route path="/" exact render={() => <HomePage viewport={viewport} />} />
-          <Route path="/portfolio/:user_id" exact component={PortfolioPage} />
-          <Route
-            path="/edit/portfolio/:portfolio_id"
-            exact
-            render={() => <PortfolioEditPage viewport={viewport} />}
-          />
-          <Route path="/project/:project_id" exact component={ProjectPage} />
-          <Route
-            path="/edit/project/:project_id?"
-            exact
-            render={() => <ProjectEditPage viewport={viewport} />}
-          />
-          <Route path="/favorite_project/:current_user_id" exact component={FavoriteProjectPage} />
-          <Route path="/search" exact component={SearchPage} />
-          <Route path="/sign_in" exact component={SignIn} />
-          <Route path="/page-not-found" component={PageNotFound} />
-          <Redirect to="/page-not-found" />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/portfolio/:user_id" exact component={Portfolio} />
+            <Route path="/edit/portfolio/:portfolio_id" exact component={PortfolioEdit} />
+            <Route path="/project/:project_id" exact component={Project} />
+            <Route path="/edit/project/:project_id?" exact component={ProjectEdit} />
+            <Route path="/favorite_project/:current_user_id" exact component={FavoriteProject} />
+            <Route path="/search" exact component={Search} />
+            <Route path="/sign_in" exact component={SignIn} />
+            <Route path="/page-not-found" component={PageNotFound} />
+            <Redirect to="/page-not-found" />
+          </Switch>
+        </Suspense>
       </EmptyContainer>
       {!isInfinityScrollPage && <FooterBar />}
     </FlexApp>
